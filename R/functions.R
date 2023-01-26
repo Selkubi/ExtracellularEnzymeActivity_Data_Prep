cab478ee301b0c0ccd4b79bb51eeedcaf47c916a
Selin Kubilay <kubilay@igb-berlin.de>
2023-01-25 18:31:47 -0800

    enzyme ratio calculation functions

--- R/functions.R
+++ R/functions.R
@@ -28,6 +28,11 @@ read_L_DOPA = function(df){
   return(measurement)
 }
 
+read_Pep = function(df){
+  measurement=read_excel(df,  range = "Z84:AD84",  col_names=paste0("rep", seq_along(1:5)))
+  return(measurement)
+}
+
 name_change = function(x) {
    substr(x$sample, 10, nchar(x$sample)-5)
 }
@@ -52,8 +57,47 @@ calculate_median = function (x) {
   return(x)
 }
 
-xyl_gly = function (x,y) {
-  apply(x[,-1], 1, median)
+calculate_xyl_gly = function (x) {
+  cbind(sample = x$Gly$sample, map2_dfr(x$Gly[,"median"],x$Xyl[,"median"],"sample", 
+                                       .f= ~.y / .x, ))
 }
+
+calculate_glu.xyl_cbh = function (x) {
+  cbind(sample = x$Gly$sample, map2_dfr(x$Gly[,7],x$Xyl[,"median"], x$Cbh[,"median"], "sample", 
+                                       .f= ~((..1 + ..2)/..3)))
+  
+}
+
+calculate_glu_pep = function (x) {
+  cbind(sample = x$Gly$sample, map2_dfr(x$Gly[,"median"],x$Pep[,"median"], "sample", 
+                               .f= ~.x /.y))
+}
+
+calculate_pep_pho = function (x) {
+  cbind(sample = x$Pep$sample, map2_dfr(x$Pep[,"median"],x$Pho[,"median"], "sample", 
+                               .f= ~.x /.y))
+}
+
+calculate_glu_nag = function (x) {
+  cbind(sample = x$Gly$sample, map2_dfr(x$Gly[,"median"], x$NAG[,"median"], "sample", 
+                               .f= ~.x /.y))
+}
+
+calculate_glu_ldopa = function (x) {
+  cbind(sample = x$Gly$sample, map2_dfr(x$Gly[,"median"], x$Ldopa[,"median"], "sample", 
+                               .f= ~.x /.y))
+}
+
+calculate_cbh_ldopa = function (x) {
+  cbind(sample = x$Cbh$sample, map2_dfr(x$Cbh[,"median"], x$Ldopa[,"median"], "sample", 
+                               .f= ~.x /.y))
+}
+
+calculate_nag_ldopa = function (x) {
+  cbind(sample = x$NAG$sample, map2_dfr(x$NAG[,"median"], x$Ldopa[,"median"], "sample", 
+                               .f= ~.x /.y))
+}
+
+
 
 
